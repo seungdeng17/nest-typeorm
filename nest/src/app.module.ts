afbd8b6feed0b95b14bdaa251b476d278ecb5dd9
seungdeng17 <seungdeng17@gmail.com>
2022-06-04 15:22:29 +0900

    feat: logging 미들웨어

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,9 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
-console.log(process.env.NODE_ENV);
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -16,4 +15,8 @@ console.log(process.env.NODE_ENV);
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}

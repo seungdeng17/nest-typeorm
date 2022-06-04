12a0de05f0dbec20436c9e5e7f75f849f0dff9d3
seungdeng17 <seungdeng17@gmail.com>
2022-06-04 14:14:44 +0900

    feat: 컨트롤러, 서비스(비지니스 로직)

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('abc')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // 요청, 응답에 대해서 알고있음
+  @Get('user') // GET /abc/user
+  async getUser(): Promise<string> {
+    return await this.appService.getUser();
   }
 }
--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+// 요청, 응답에 대해서는 모름
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getUser() {
+    // 트랜잭션 단위의 비지니스 로직
+    // const user = await User.findOne();
+    // return user;
+    return 'hello user';
   }
 }

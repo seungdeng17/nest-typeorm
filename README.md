a9f2f0c415754fbd0652b08191728953df760806
seungdeng17 <seungdeng17@gmail.com>
2022-06-06 16:20:49 +0900

    feat: logged in, not logged in 미들웨어

--- README.md
+++ README.md
@@ -31,3 +31,6 @@ http exception filter (useGlobalFilters)
 class-validator (useGlobalPipes)
 
 @UseGuards 권한체크 (auth guard > strategy > serializer)
+
+lify cycle
+![nest](https://velog.velcdn.com/images%2Fharon%2Fpost%2Fe2587453-9aa2-4f2d-9ae4-0c8c024ed42f%2Fimage.png)
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -20,6 +20,8 @@ import { UserDto } from 'src/common/dto/user.dto';
 import { User } from '../common/decorators/user.decorator';
 import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { LoggedInGuard } from 'src/auth/logged-in.guard';
+import { NotLoggedInGuard } from 'src/auth/not-logged-in.guard';
 
 @UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('USER')
@@ -39,9 +41,10 @@ export class UsersController {
   @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@User() user) {
-    return user;
+    return user || false;
   }
 
+  @UseGuards(new NotLoggedInGuard())
   @ApiOperation({ summary: '회원가입' })
   @Post()
   async join(@Body() body: JoinRequestDto) {
@@ -53,12 +56,13 @@ export class UsersController {
     type: UserDto,
   })
   @ApiOperation({ summary: '로그인' })
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(new LocalAuthGuard())
   @Post('login')
   logIn(@User() user) {
     return user;
   }
 
+  @UseGuards(new LoggedInGuard())
   @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
